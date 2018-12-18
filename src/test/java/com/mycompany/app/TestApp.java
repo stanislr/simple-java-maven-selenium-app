50765ce8e51f10f0d92a5cdedb6c73021633ddf8
umangsaltuniv <umang@saltuniv.com>
2018-12-18 14:36:54 +0530

    MyCommit

--- src/test/java/com/mycompany/app/TestApp.java
+++ src/test/java/com/mycompany/app/TestApp.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest
+public class TestApp
 {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
